a0edb7288155fa70e376d867b4469d41e4fc9627
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-05-24 22:05:18 +0100

    set up

---
+++ README.md
@@ -0,0 +1,1 @@
+An idea for the engine in the 5 minutes catch-up app (final project with Makers Academy)
---
+++ spec/FriendSpec.js
@@ -0,0 +1,26 @@
+describe('Friend', function(){
+  var friend;
+  beforeEach(function(){
+    friend = new Friend;
+  });
+
+  xit('can have the 5mcu app', function(){
+
+  });
+
+  xit('can be on incognito mode', function(){
+
+  });
+
+  xit('authorise 5mcu to check his location if authorised', function(){
+
+  });
+
+  xit('does not authorise 5mcu to check his location if not authorised', function(){
+    
+  });
+
+  xit('has a blacklist of his/her own', function(){
+
+  });
+});
